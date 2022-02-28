48e496eee42441f46eff2e5a9799a17bc3acf78b
Noah Sharum <37598184+nsharum18@users.noreply.github.com>
2022-02-28 13:36:49 -0500

    Update SimpleAuto.java

--- autocommands/SimpleAuto.java
+++ autocommands/SimpleAuto.java
@@ -9,8 +9,11 @@ import frc.robot.commands.*;
 public class SimpleAuto extends SequentialCommandGroup {
 
 
-    public SimpleAuto(DriveSubsystem m_drive, ArmSubsystem m_arm, WristSubsystem m_wrist, IntakeSubsystem m_intake) {
+    public SimpleAuto(DriveSubsystem m_drive, ArmSubsystem m_arm, WristSubsystem m_wrist, IntakeSubsystem m_intake, ClimberSubsystem m_climb) {
         addCommands(
+
+        //sets start config
+        new setStartConfig(m_arm, m_drive, m_intake, m_wrist, m_climb),
 
         //drive to a forward set number, at a certain speed
         new DriveCommand(true, Constants.FORWARD_DRIVE, Constants.AUTO_SPEED, m_drive),
