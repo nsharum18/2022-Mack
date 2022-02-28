b2deb19bbe938c72942141ba8f2d85648e7a687d
Noah Sharum <37598184+nsharum18@users.noreply.github.com>
2022-02-28 09:41:29 -0500

    Add files via upload

---
+++ autocommands/ActualAuto.java
@@ -0,0 +1,28 @@
+package frc.robot.autocommands;
+
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.subsystems.*;
+import frc.robot.commands.*;
+
+public class ActualAuto extends SequentialCommandGroup {
+
+
+    public ActualAuto(DriveSubsystem m_drive, ArmSubsystem m_arm, WristSubsystem m_wrist, IntakeSubsystem m_intake) {
+        addCommands(
+
+        //score position
+        new ScorePosition(m_arm, m_wrist),
+
+        //intake scoring
+        new Score(m_intake),
+
+         //drive to a backward set number, at a certain speed
+        new DriveCommand(false, Constants.BACKUP_DRIVE * -1, Constants.AUTO_SPEED * -1, m_drive)
+
+
+        );
+      }
+    
+}
---
+++ autocommands/SimpleAuto.java
@@ -0,0 +1,28 @@
+package frc.robot.autocommands;
+
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.subsystems.*;
+import frc.robot.commands.*;
+
+public class SimpleAuto extends SequentialCommandGroup {
+
+
+    public SimpleAuto(DriveSubsystem m_drive, ArmSubsystem m_arm, WristSubsystem m_wrist, IntakeSubsystem m_intake) {
+        addCommands(
+
+        //drive to a forward set number, at a certain speed
+        new DriveCommand(true, Constants.FORWARD_DRIVE, Constants.AUTO_SPEED, m_drive),
+
+        //score position
+        new ScorePosition(m_arm, m_wrist),
+
+        //intake scoring
+        new Score(m_intake)
+
+
+        );
+      }
+    
+}
