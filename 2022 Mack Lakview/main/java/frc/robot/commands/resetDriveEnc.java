2f4fa671179cf0f516f732c635acdd32ebc42d3b
Noah Sharum <37598184+nsharum18@users.noreply.github.com>
2022-03-22 09:51:11 -0400

    Add files via upload

---
+++ 2022 Mack Lakview/main/java/frc/robot/commands/resetDriveEnc.java
@@ -0,0 +1,41 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.*;
+
+
+public class resetDriveEnc extends CommandBase {
+    
+    private final DriveSubsystem drive;
+    private final ArmSubsystem arm;
+    private final WristSubsystem wrist;
+
+    public resetDriveEnc(DriveSubsystem m_drive, ArmSubsystem m_arm, WristSubsystem m_wrist) {
+        drive = m_drive;
+        arm = m_arm;
+        wrist = m_wrist;
+
+        addRequirements(m_drive);
+    }
+
+    public void initialize() {
+        drive.resetEncoders();
+        arm.resetArmEnc();
+        wrist.resetWristEnc();
+        
+    }
+
+    public void execute() {
+        drive.resetEncoders();
+        arm.resetArmEnc();
+        wrist.resetWristEnc();
+
+    }
+
+    public void end(boolean inturrupted) {
+    }
+
+    public boolean isFinished() {
+        return drive.getLMasterEnc() == 0;
+    }
+}
