58733d9801aae11e04302a7771b1c9c675fcb899
Noah Sharum <37598184+nsharum18@users.noreply.github.com>
2022-03-25 09:08:03 -0400

    Add files via upload

---
+++ 2022 Mack Lakview/java/frc/robot/autocommands/RightTwoBallAuto.java
@@ -0,0 +1,45 @@
+package frc.robot.autocommands;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.commands.Intake;
+import frc.robot.commands.IntakePosition;
+import frc.robot.commands.ScorePosition;
+import frc.robot.subsystems.*;
+
+public class RightTwoBallAuto extends SequentialCommandGroup {
+
+    public RightTwoBallAuto(DriveSubsystem m_drive, ArmSubsystem m_arm, WristSubsystem m_wrist, IntakeSubsystem m_intake, LClimbSubsystem l_climb, RClimbSubsystem r_climb) {
+
+      Trajectory r2b1 = m_drive.generateTrajectroy(Constants.r2b1);
+      Trajectory r2b2 = m_drive.generateTrajectroy(Constants.r2b2);
+
+        addCommands(
+/*
+            new setStartConfig(m_arm, m_drive, m_intake, m_wrist, l_climb, r_climb),
+
+            new AutoWristDown(m_wrist),
+
+            new AutoScore(m_intake),
+*/
+            new InstantCommand(() -> m_drive.resetOdometry(r2b1.getInitialPose())),
+
+           // new ParallelDeadlineGroup(
+              new FollowTrajectory(m_drive, r2b1),
+              //new IntakePosition(m_arm, m_wrist),
+              //new Intake(m_intake)
+            //),
+
+            //new ParallelDeadlineGroup(
+              new FollowTrajectory(m_drive, r2b2)
+              //new ScorePosition(m_arm, m_wrist)
+            //),
+
+            //new AutoScore(m_intake)
+
+        );
+      }
+}
